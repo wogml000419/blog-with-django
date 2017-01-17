39fffa103153e1219fb11e200a54793afc582acb
wogml000419 <wogml0123456@naver.com>
2017-01-17 21:10:00 +0900

    글 수정 기능 추가

--- blog/views.py
+++ blog/views.py
@@ -23,3 +23,17 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form' : form})
+
+def post_edit(request, key):
+    post = get_object_or_404(Post, pk = key)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance = post)
+        if form.is_valid():
+            post = form.save()
+            #post.author = request.user
+            #post.published_date = timezone.now()
+            #post.save()
+            return redirect(post_detail, key = post.pk)
+    else:
+        form = PostForm(instance = post)
+    return render(request, 'blog/post_edit.html', { 'form' : form })
